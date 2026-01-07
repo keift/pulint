df7bec6816cabed651c35b5729aa132195778274
fir4tozden <fir4tozden@gmail.com>
2026-01-07 18:00:02 +0300

    v1.0.2

--- src/defaults/RulintOptions.default.ts
+++ src/defaults/RulintOptions.default.ts
@@ -2,7 +2,7 @@ import TSESLint from 'typescript-eslint';
 import ESLintJS from '@eslint/js';
 
 import type { ESLintConfig } from '../types/ESLintConfig.type';
-import type { PulintOptions } from '../types/PulintOptions.type';
+import type { RulintOptions } from '../types/RulintOptions.type';
 
 export const js_rules: { configs: ESLintConfig['rules']; custom: ESLintConfig['rules'] } = {
   configs: {
@@ -141,7 +141,7 @@ export const ts_rules: { configs: ESLintConfig['rules']; custom: ESLintConfig['r
   }
 };
 
-export const PulintOptionsDefault: PulintOptions = {
+export const RulintOptionsDefault: RulintOptions = {
   disabled: false,
 
   ignores: ['**/dist'],
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,3 @@
-export * from './utils/Pulint.util';
+export * from './utils/Rulint.util';
 
 export type * from './exports/Types.export';
